064fe0a64b7b50ac57489f62cc57ffa28b4b5e21
hlandim <hlandim.s@gmail.com>
2017-01-12 23:59:00 -0200

    adding Create user service

--- app/src/main/java/br/com/hlandim/supermarket/login/data/SessionService.java
+++
@@ -1,8 +0,0 @@
-package br.com.hlandim.supermarket.login.data;
-
-/**
- * Created by hlandim on 10/01/17.
- */
-
-public interface SessionService {
-}
--- app/src/main/java/br/com/hlandim/supermarket/login/viewModel/LoginViewModelContract.java
+++
@@ -1,9 +0,0 @@
-package br.com.hlandim.supermarket.login.viewModel;
-
-/**
- * Created by hlandim on 10/01/17.
- */
-
-public interface LoginViewModelContract {
-    void showProgress(boolean show);
-}
