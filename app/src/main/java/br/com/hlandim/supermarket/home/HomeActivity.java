a5f350fed61707e84d4140373b0730f9dd198c7f
hlandim <hlandim.s@gmail.com>
2017-01-14 22:23:48 -0200

    Adding ScrollView

--- app/src/main/java/br/com/hlandim/supermarket/home/HomeActivity.java
+++ app/src/main/java/br/com/hlandim/supermarket/home/HomeActivity.java
@@ -1,15 +1,36 @@
 package br.com.hlandim.supermarket.home;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 import br.com.hlandim.supermarket.R;
+import br.com.hlandim.supermarket.util.PageAnimation;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private static final String TAG_FRAGMENT = "my_fragment_tag";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+
+    private void changeFragment(Fragment fragment, PageAnimation pageAnimation) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+        //Configure animation
+        if (pageAnimation != null) {
+            int enter = pageAnimation.getInTransition();
+            int exit = pageAnimation.getOutTransition();
+            if (enter > 0 && exit > 0) {
+                fragmentTransaction.setCustomAnimations(enter, exit);
+            }
+        }
+
+        fragmentTransaction.replace(R.id.container_fragment, fragment, TAG_FRAGMENT).commit();
     }
 }
